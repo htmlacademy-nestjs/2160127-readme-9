6e1470a47e4ceeae5688e300d89195b7e130a624
Anur85 <an-nv85@yandex.ru>
2025-05-13 16:42:36 +0500

    2.14. Зарегистрирует BlogUserRepository и BlogUserFactory в качестве провайдера.

--- project/libs/user/blog-user/src/blog-user-module/blog-user.repository.ts
+++ project/libs/user/blog-user/src/blog-user-module/blog-user.repository.ts
@@ -1,8 +1,10 @@
 import { BaseMemoryRepository } from '@project/data-access';
+import { Injectable } from '@nestjs/common';
 
 import { BlogUserEntity } from './blog-user.entity';
 import { BlogUserFactory } from './blog-user.factory';
 
+@Injectable()
 export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {
   constructor(entityFactory: BlogUserFactory) {
     super(entityFactory);
