3e3f2291a93942d8f02bae9ed2c45bed6fa53f3c
Anur85 <an-nv85@yandex.ru>
2025-05-13 18:25:42 +0500

    2.25. Добавит обработчик маршрута /register.

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -14,7 +14,7 @@ export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) { }
-  public async register(dto: CreateUserDto) {
+  public async register(dto: CreateUserDto): Promise<BlogUserEntity> {
     const {email, firstname, lastname, password, dateBirth} = dto;
 
     const blogUser = {
