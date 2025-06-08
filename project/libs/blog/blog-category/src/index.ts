2dd5b4e02b2ed2fc89ee2758320444ba012ece02
Anur <an-nv85@yandex.ru>
2025-06-08 10:38:09 +0500

    add dto & rdo in blog-post

--- project/libs/blog/blog-category/src/index.ts
+++ project/libs/blog/blog-category/src/index.ts
@@ -1,1 +1,5 @@
 export { BlogCategoryModule } from './blog-category-module/blog-category.module';
+export { CategoryRdo } from './blog-category-module/rdo/category.rdo';
+export { CreateCategoryDto } from './blog-category-module/dto/create-category.dto';
+export { UpdateCategoryDto } from './blog-category-module/dto/update-category.dto';
+
