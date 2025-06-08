8d73e79091cbbb67fb4a8636c2b7b371bb48063a
Anur <an-nv85@yandex.ru>
2025-06-08 08:39:08 +0500

    add blog-post dll && add PaginationResult, SortDirection

--- project/libs/shared/core/src/lib/types/post.interface.ts
+++ project/libs/shared/core/src/lib/types/post.interface.ts
@@ -1,4 +1,5 @@
 import { Category } from './category.interface';
+import { Comment } from './comment.interface';
 
 export interface Post {
   id?: string;
@@ -8,5 +9,5 @@ export interface Post {
   createdAt?: Date;
   updatedAt?: Date;
   userId: string;
-  comments: Comment[];
+  comments: Comment[]; 
 }
