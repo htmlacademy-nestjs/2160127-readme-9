3a9fc0e74aac42ae49e72889fbb579eb79e87622
Anur <an-nv85@yandex.ru>
2025-06-08 21:53:14 +0500

    add getJwtOptions

--- project/libs/user/config/src/user-config.module.ts
+++ project/libs/user/config/src/user-config.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import applicationConfig from './configurations/app.config';
 import mongoConfig  from './configurations/mongo.config'
-import jwtConfig from './configurations/jwt.config';
+import jwtConfig from './configurations/jwt/jwt.config';
 
 const ENV_USERS_FILE_PATH = 'apps/user/user.env';
 
