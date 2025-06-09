adcab58e3adbccedae07f5cb987cf68fcf16b80b
Anur <an-nv85@yandex.ru>
2025-06-09 22:48:40 +0500

    add FileUploaderModule

--- project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.controller.ts
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.controller.ts
@@ -1,12 +1,14 @@
 import 'multer';
 import { Express } from 'express';
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, Logger } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { FileUploaderService } from './file-uploader.service';
+import { cwd } from 'process';
 
 @Controller('files')
 export class FileUploaderController {
+    private readonly logger = new Logger(FileUploaderController.name);
   constructor(
     private readonly fileUploaderService: FileUploaderService,
   ) {}
@@ -14,6 +16,9 @@ export class FileUploaderController {
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
   public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    this.logger.debug('in /upload');
+    console.log(cwd);
+    console.log(file);
     return this.fileUploaderService.saveFile(file);
   }
 }
